bbc8b969927dab502ab817c9d08cd2924e43e907
Mike Kroutikov <pgmmpk@gmail.com>
2014-05-11 13:23:45 -0400

    new example - testing sensor readings

---
+++ examples/sensors.js
@@ -0,0 +1,30 @@
+
+var config = require('../config');
+
+require('../robot/sensors')(config, function(err, sensors) {
+
+    if (err) {
+        return console.log('ERROR:', err);
+    }
+
+    sensors.start();
+
+    sensors.read();
+
+    var timer = sensors.timer;
+    var ticks_left = sensors.ticks_left;
+    var ticks_right = sensors.ticks_right;
+
+    setInterval(ontimer, 100);
+
+    function ontimer() {
+        sensors.read();
+
+        if (ticks_left != sensors.ticks_left || ticks_right != sensors.ticks_right) {
+            ticks_left = sensors.ticks_left;
+            ticks_right = sensors.ticks_right;
+
+            console.log(ticks_left, ticks_right, sensors.speed_left, sensors.speed_right);
+        }
+    }
+});
