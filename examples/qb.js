d2f3928ca1a7d32cc3af98bc03ecaecd8bfade6e
Mike Kroutikov <pgmmpk@gmail.com>
2014-05-11 19:38:18 +0000

    cleanup

--- examples/qb.js
+++ examples/qb.js
@@ -8,9 +8,16 @@ require('../robot/qb')(config, function(err, qb) {
 
     qb.start();
 
-    setInterval(function() {
+    var worker = setInterval(function() {
         qb.onTimer();
     }, 10);
 
-    qb.setSpeed(40, 0);
+    qb.setSpeed(60, 60);
+
+    setTimeout(function() {
+	qb.setSpeed(0, 0);
+	setTimeout(function() {
+	    clearInterval(worker);
+	}, 1000);
+    }, 2000);
 });
