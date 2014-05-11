44c803e9b1c85dcab4b12bc20e40d721c6a402d5
Mike Kroutikov <pgmmpk@gmail.com>
2014-05-11 15:59:23 -0400

    fixing web service for robot

--- robot/index.js
+++ robot/index.js
@@ -1,42 +1,52 @@
 var config = require('../config');
 
-function QbService(config, callback) {
+var status = undefined;
+var qb = undefined;
 
-    require('./qb')(config, function(err, qb) {
+require('./qb')(config, function(err, qb_) {
+    status = 'Initializing QuickBot...';
 
-        if (err) {
-	    return collabck(err);
-	}
+    if (err) {
+        status = 'ERROR: ' + err;
+        return;
+    }
 
-        qb.start();
-        var worker = setInterval(qb.onTimer, 10);
-
-	var service = {};
-
-        service.speed = function(val) {
-            if (val === undefined) {
-                return qb.getSpeed();
-            } else {
-                qb.setSpeed(val.speed_left, val.speed_right);
-            }
-        };
-
-        service.ticks = function() {
-            return qb.getTicks();
-        };
-
-        service.ir_distance = function() {
-            return qb.getIrDistance();
-        };
-
-        service.ir_raw = function() {
-            return qb.getIr();
-        };
-
-        service.motors = qb.motors;
-
-        service.sensors = qb.sensors;
-
-	callback(null, service);
+    qb = qb_;
+    qb.start();
+    setInterval(qb.onTimer, 10);
 });
+
+function ensureLoaded() {
+    if (qb === undefined) {
+        throw new Error(status);
+    }
+}
+
+module.exports = {
+
+    service.speed = function(val) {
+        if (val === undefined) {
+            return qb.getSpeed();
+        } else {
+            qb.setSpeed(val.speed_left, val.speed_right);
+        }
+    };
+
+    service.ticks = function() {
+
+        return qb.getTicks();
+    };
+
+    service.ir_distance = function() {
+        return qb.getIrDistance();
+    };
+
+    service.ir_raw = function() {
+        return qb.getIr();
+    };
+
+    service.motors = qb.motors;
+
+    service.sensors = qb.sensors;
+};
 
