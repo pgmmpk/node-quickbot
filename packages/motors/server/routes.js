046b06783c774e02070da53edc369599aa1a52cc
Mike Kroutikov <pgmmpk@gmail.com>
2014-05-19 19:19:41 -0400

    cleanup

--- packages/motors/server/routes.js
+++ packages/motors/server/routes.js
@@ -2,26 +2,29 @@ module.exports = ['mean.app', 'qbapi', function(app, qbapi) {
 
     var motors = undefined;
 
-    app.route('/api/motors/run')
-        .post(function(req, res) {
-            var torqueLeft = +req.body.torqueLeft;
-            var torqueRight = +req.body.torqueRight;
-            
-            if (!motors) {
-                qbapi.motors(qbapi.defaultConfig, function(err, m) {
-                    if (err) {
-                        console.log("Failed to init motors:", err);
-                        return res.send(500);
-                    }
-    
-                    motors = m;
-                    motors.run(torqueLeft, torqueRight);
-                    return res.json({status: 'OK'});
-                });
-            } else {
-                motors.run(torqueLeft, torqueRight);
-                return res.json({status: 'OK'});
-            }
-        });
+    app.route('/api/motors/run').post(function(req, res) {
+        var torqueLeft = +req.body.torqueLeft;
+        var torqueRight = +req.body.torqueRight;
+        
+        if (!motors) {
+            qbapi.motors(qbapi.defaultConfig, function(err, m) {
+                if (err) {
+                    console.log("Failed to init motors:", err);
+                    return res.send(500);
+                }
+
+                motors = m;
+                
+                runMotors();
+            });
+        } else {
+            runMotors();
+        }
+        
+        function runMotors() {
+            motors.run(torqueLeft, torqueRight);
+            return res.json({status: 'OK'});
+        }
+    });
 
 }];
