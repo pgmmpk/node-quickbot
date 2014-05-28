e4c6a63e977ebc00da7703c2d593b20836bce706
Mike Kroutikov <pgmmpk@gmail.com>
2014-05-27 20:17:13 -0400

    more d3 fixes

--- packages/sensors/public/d3.js
+++ packages/sensors/public/d3.js
@@ -6,27 +6,34 @@
         return {
             restrict: 'E',
             scope: {
+                width: '@',
+                height: '@',
+
                 data: '='
             },
             link: function(scope, element, attrs) {
                 var svg = d3.select(element[0]).append('svg').style('width', '100%');
 
-                var xScale = d3.scale.linear().domain([0, 150]).range([0, 300]);
-                var yScale = d3.scale.linear().domain([0, 4096]).range([0, 300]);
+                var width = attrs.width || 300;
+                var height = attrs.height || 300;
+
+                var xScale = d3.scale.linear().domain([0, 150]).range([0, width]);
+                var yScale = d3.scale.linear().domain([4096, 0]).range([0, height]);
 
                 function update(selection) {
                     selection.attr('height', function(d, i) {
-                        return yScale(d);
-                    }).attr('width', 20).attr('x', function(d, i) {
-                        return i * 30;
-                    }).attr('y', 0)
+                        return height - yScale(d);
+                    }).attr('width', xScale(20))
+                    .attr('x', function(d, i) {
+                        return xScale(i * 30);
+                    }).attr('y', yScale(0))
                     .attr('fill', 'green');
                 }
 
                 scope.$watch('data', function() {
                     var data = scope.data || [];
                     var rects = svg.selectAll('rect').data(data);
-                    
+
                     rects.enter().append('rect').call(update);
                     rects.exit().remove();
                     rects.call(update);
